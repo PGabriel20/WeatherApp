d411701f6cce0badb059323d26c78ad61d6f2bd9
PGabriel20 <bielporreca@gmail.com>
2021-09-01 10:39:21 -0300

    fix: calling date formatter method

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
-          <div class="date">Wednsday 20 September 2021</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
 
         <div class="weather-box">
@@ -65,7 +65,7 @@ export default {
       let date = d.getDate();
       let month = months[d.getMonth()];
       let year = d.getFullYear();
-      
+
       return `${day} ${date} ${month} ${year}`;
     }
   }
