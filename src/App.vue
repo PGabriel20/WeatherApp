a43e382522234582d4a5d2a952cf5bb3099533ff
PGabriel20 <bielporreca@gmail.com>
2021-09-01 11:08:26 -0300

    fix: v-if inside main components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm': ''">
+  <div id="app" :class="(typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm': '')">
     <main>
       <div class="search-box">
         <input 
@@ -13,7 +13,7 @@
         >
       </div>
 
-      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+      <div class="weather-wrap" v-if="(typeof weather.main != 'undefined')">
         <div class="location-box">
           <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
           <div class="date">{{ dateBuilder() }}</div>
